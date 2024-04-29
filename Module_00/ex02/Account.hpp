3a82ec73a026d551a0a36f7a97743c40b60cae49
zelabbas <zakariaelabbassi2002@gmail.com>
2024-04-29 16:24:43 +0100

    header file

---
+++ Module_00/ex02/Account.hpp
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zelabbas <zelabbas@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/29 11:32:57 by zelabbas          #+#    #+#             */
+/*   Updated: 2024/04/29 16:19:47 by zelabbas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#ifndef __ACCOUNT_H__
+#define __ACCOUNT_H__	
+
+// ************************************************************************** //
+//                               Account Class                                //
+// ************************************************************************** //
+
+class Account 
+{
+	public:
+
+		typedef Account		t;
+
+		static int	getNbAccounts( void );
+		static int	getTotalAmount( void );
+		static int	getNbDeposits( void );
+		static int	getNbWithdrawals( void );
+		static void	displayAccountsInfos( void );
+
+		Account( int initial_deposit );
+		~Account( void );
+
+		void	makeDeposit( int deposit );
+		bool	makeWithdrawal( int withdrawal );
+		int		checkAmount( void ) const;
+		void	displayStatus( void ) const;
+
+
+	private:
+
+		static int	_nbAccounts;
+		static int	_totalAmount;
+		static int	_totalNbDeposits;
+		static int	_totalNbWithdrawals;
+
+		static void	_displayTimestamp( void );
+
+		int				_accountIndex;
+		int				_amount;
+		int				_nbDeposits;
+		int				_nbWithdrawals;
+
+		Account( void );
+
+};
+
+
+
+// ************************************************************************** //
+// vim: set ts=4 sw=4 tw=80 noexpandtab:                                      //
+// -*- indent-tabs-mode:t;                                                   -*-
+// -*- mode: c++-mode;                                                       -*-
+// -*- fill-column: 75; comment-column: 75;                                  -*-
+// ************************************************************************** //
+
+
+#endif /* __ACCOUNT_H__ */
