863671198f5eab9bfba9f55399072ad07ec1cb9e
zelabbas <zakariaelabbassi2002@gmail.com>
2024-05-02 14:26:15 +0100

    add the file that conatain the defenition of all methods in class zombie

---
+++ Module_01/ex00/newZombie.cpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   newZombie.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zelabbas <zelabbas@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/29 20:51:33 by zelabbas          #+#    #+#             */
+/*   Updated: 2024/04/29 20:54:02 by zelabbas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+Zombie* newZombie( std::string name )
+{
+	Zombie *obj = new Zombie;
+	obj->setName(name);
+	return (obj);
+}
