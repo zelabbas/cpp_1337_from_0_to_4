8822aa6db5d95df776cca3bdc1fb70fdfaa9ddfc
zelabbas <zakariaelabbassi2002@gmail.com>
2024-05-02 17:00:48 +0100

    define class Harl

---
+++ Module_01/ex05/Harl.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zelabbas <zelabbas@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/02 10:36:29 by zelabbas          #+#    #+#             */
+/*   Updated: 2024/05/02 15:55:11 by zelabbas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+#define HARL_HPP
+#define RED_COLOR "\033[1;31m"
+#include <iostream>
+
+class Harl
+{
+	private:
+		void debug( void );
+		void info( void );
+		void warning( void );
+		void error( void );
+	public:
+		Harl();
+		~Harl();
+		void complain( std::string level );
+};
+
+#endif
