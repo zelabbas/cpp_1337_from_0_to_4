cf08f22700867f594dd9434a009c53187cebf75c
zelabbas <zakariaelabbassi2002@gmail.com>
2024-05-02 14:43:33 +0100

    header file conatain the definition class

---
+++ Module_01/ex00/Zombie.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zelabbas <zelabbas@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/29 20:37:23 by zelabbas          #+#    #+#             */
+/*   Updated: 2024/05/02 14:36:47 by zelabbas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+#include <iostream>
+
+class Zombie
+{
+	private:
+		std::string	name;
+	public:
+					Zombie();
+					~Zombie();
+		void		setName(std::string);
+		std::string	getName(void);
+		void announce(void);
+};
+
+Zombie* newZombie( std::string name );
+void randomChump( std::string name );
+
+#endif	
