093ea8815cb9cacdc9a0a59cfe39d4b620829e11
zelabbas <zakariaelabbassi2002@gmail.com>
2024-05-02 14:58:35 +0100

    add the header file

---
+++ Module_01/ex04/replace.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   replace.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zelabbas <zelabbas@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/01 16:02:22 by zelabbas          #+#    #+#             */
+/*   Updated: 2024/05/02 09:55:39 by zelabbas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef	REPLACE_HPP
+#define REPLACE_HPP
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+std::string	replace_string(std::string line,std::string str_toreplace, std::string str_replace);
+
+#endif
