2383b8fea06f9b68d8f7f199518467c05e62c365
zelabbas <zakariaelabbassi2002@gmail.com>
2024-05-02 14:57:11 +0100

    add the main program

---
+++ Module_01/ex03/main.cpp
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zelabbas <zelabbas@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/01 15:12:22 by zelabbas          #+#    #+#             */
+/*   Updated: 2024/05/02 13:52:12 by zelabbas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "HumanA.hpp"
+#include "HumanB.hpp"
+
+// void	leak(){system("leaks human");}
+
+int main(int ac, char **av)
+{
+	// atexit(leak);
+	(void)av;
+	if (ac != 1)
+	{
+		std::cerr << "The program don't accepte arguments." << std::endl;
+		return (0);
+	}
+	{
+		Weapon club = Weapon("crude spiked club");
+		HumanA bob("Bob", club);
+		bob.attack();
+		club.setType("some other type of club");
+		bob.attack();
+	}
+	{
+		Weapon club = Weapon("crude spiked club");
+		HumanB jim("Jim");
+		jim.setWeapon(club);
+		jim.attack();
+		club.setType("some other type of club");
+		jim.attack();
+		jim.getWeapon();
+	}
+	return 0;
+}
