4e1a6a5799750d72ca0ccd8e851282f72f40bc31
zelabbas <zakariaelabbassi2002@gmail.com>
2024-05-03 17:15:35 +0100

    Done

--- Module_01/ex03/HumanB.cpp
+++ Module_01/ex03/HumanB.cpp
@@ -6,7 +6,7 @@
 /*   By: zelabbas <zelabbas@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 16:03:47 by zelabbas          #+#    #+#             */
-/*   Updated: 2024/05/03 16:54:46 by zelabbas         ###   ########.fr       */
+/*   Updated: 2024/05/03 17:12:16 by zelabbas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,4 +54,3 @@ std::string	HumanB::getName(void)
 {
 	return (this->name);
 }
-
