af91cbbcfef447b8405f9ab5bd70390d452e05ad
zelabbas <zakariaelabbassi2002@gmail.com>
2024-05-03 10:29:03 +0100

    add a macro needed for reset color

--- Module_01/ex05/Harl.hpp
+++ Module_01/ex05/Harl.hpp
@@ -6,13 +6,15 @@
 /*   By: zelabbas <zelabbas@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 10:36:29 by zelabbas          #+#    #+#             */
-/*   Updated: 2024/05/02 15:55:11 by zelabbas         ###   ########.fr       */
+/*   Updated: 2024/05/03 10:27:02 by zelabbas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HARL_HPP
 #define HARL_HPP
 #define RED_COLOR "\033[1;31m"
+#define RESET_COLOR "\033[0m"
+
 #include <iostream>
 
 class Harl
