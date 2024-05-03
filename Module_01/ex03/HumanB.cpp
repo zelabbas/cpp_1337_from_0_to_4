9968105693effa078c384c1a782144e0bd91ed3f
zelabbas <zakariaelabbassi2002@gmail.com>
2024-05-03 16:55:28 +0100

    fix an error in method attack

--- Module_01/ex03/HumanB.cpp
+++ Module_01/ex03/HumanB.cpp
@@ -6,7 +6,7 @@
 /*   By: zelabbas <zelabbas@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 16:03:47 by zelabbas          #+#    #+#             */
-/*   Updated: 2024/05/01 15:12:11 by zelabbas         ###   ########.fr       */
+/*   Updated: 2024/05/03 16:54:46 by zelabbas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 HumanB::HumanB(std::string name)
 {
 	this->name = name;
+	this->wp_B = NULL;
 }
 
 HumanB::~HumanB()
@@ -24,8 +25,14 @@ HumanB::~HumanB()
 
 void	HumanB::attack(void)
 {
-	std::cout << this->name << " attacks with their " 
-		<< this->wp_B->getType() << std::endl;
+	if (this->wp_B)
+		std::cout << this->name << " attacks with their " 
+			<< this->wp_B->getType() << std::endl;
+	else
+	{
+		std::cout << "there's no Weapon , creat first a Weapon and try again." << std::endl;
+		exit(1);
+	}
 }
 
 void	HumanB::setWeapon(Weapon& weapon)
