d378c4eb759311e1c964dbcad19983ea8f4b51d0
zelabbas <zakariaelabbassi2002@gmail.com>
2024-05-04 16:10:26 +0100

    protect in case faild allocation memory

--- Module_01/ex00/newZombie.cpp
+++ Module_01/ex00/newZombie.cpp
@@ -6,7 +6,7 @@
 /*   By: zelabbas <zelabbas@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 20:51:33 by zelabbas          #+#    #+#             */
-/*   Updated: 2024/04/29 20:54:02 by zelabbas         ###   ########.fr       */
+/*   Updated: 2024/05/04 16:09:49 by zelabbas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,12 @@
 
 Zombie* newZombie( std::string name )
 {
-	Zombie *obj = new Zombie;
+	Zombie *obj = new(std::nothrow) Zombie;
+	if (!obj)
+	{
+		std::cout << "Faild to allocate memory." << std::endl;
+		exit(1);
+	}
 	obj->setName(name);
 	return (obj);
 }
