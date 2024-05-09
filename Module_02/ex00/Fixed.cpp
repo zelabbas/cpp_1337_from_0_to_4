93de508fbcc27e8671e8550d3bc2d194c8e49e33
zelabbas <zakariaelabbassi2002@gmail.com>
2024-05-09 20:49:03 +0100

    define all methods

---
+++ Module_02/ex00/Fixed.cpp
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zelabbas <zelabbas@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/06 13:40:30 by zelabbas          #+#    #+#             */
+/*   Updated: 2024/05/08 15:07:35 by zelabbas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+Fixed::Fixed(void)
+{
+	this->fixed_point = 0;
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed& obj)
+{
+	if (this != &obj)
+		this->fixed_point = obj.fixed_point;
+	std::cout << "Copy constructor called" << std::endl;
+}
+
+Fixed::~Fixed(void)
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+Fixed& Fixed::operator=(const Fixed& obj)
+{
+	if (this != &obj)
+		this->fixed_point = obj.getRawBits();
+	std::cout << "Copy assignment operator called" << std::endl;
+	return (*this);
+}
+
+int Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->fixed_point);
+}
+
+void	Fixed::setRawBits(int const raw)
+{
+	std::cout << "setRawBits member function called" << std::endl;
+	this->fixed_point = raw;
+}
