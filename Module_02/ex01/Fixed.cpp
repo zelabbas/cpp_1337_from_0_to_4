e070e1944852e6ca6c6114df00c0d4ea76a74cfc
zelabbas <zakariaelabbassi2002@gmail.com>
2024-05-09 20:54:34 +0100

    define the new methods that added in class

---
+++ Module_02/ex01/Fixed.cpp
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zelabbas <zelabbas@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/06 13:40:30 by zelabbas          #+#    #+#             */
+/*   Updated: 2024/05/09 20:52:29 by zelabbas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+Fixed::fractional = 8;
+
+Fixed::Fixed(void)
+{
+	this->fixed_point = 0;
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const int fixedpoint)
+{
+	this->fixed_point = fixedpoint << fractional;
+	std::cout << "Int constructor called" << std::endl;
+}
+
+Fixed::Fixed(const float floatpoint)
+{
+	this->fixed_point = (int)roundf(floatpoint * (1 << this->fractional));
+	std::cout << "Float constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed& obj)
+{
+	if (this != &obj)
+		this->fixed_point = obj.fixed_point;
+	std::cout << "Copy constructor called" << std::endl;
+}
+
+Fixed::~Fixed(void)
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+Fixed& Fixed::operator=(const Fixed& obj)
+{
+	if (this != &obj)
+		this->fixed_point = obj.getRawBits();
+	std::cout << "Copy assignment operator called" << std::endl;
+	return (*this);
+}
+
+int Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->fixed_point);
+}
+
+void	Fixed::setRawBits(int const raw)
+{
+	std::cout << "setRawBits member function called" << std::endl;
+	this->fixed_point = raw;
+}
+
+int	Fixed::toInt(void) const
+{
+	return (this->fixed_point >> fractional);
+}
+
+float	Fixed::toFloat(void) const
+{
+	return ((float)(this->fixed_point) / (1 << fractional));
+}
+
+std::ostream	&operator<< (std::ostream& os, const Fixed& obj)
+{
+	os << obj.toFloat();
+	return (os);
+}
