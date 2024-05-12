1d4a5408a847f7e289b8bbc295941046060c2bba
zelabbas <zakariaelabbassi2002@gmail.com>
2024-05-12 16:33:06 +0100

    update the main program

--- Module_00/ex00/megaphone.cpp
+++ Module_00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: zelabbas <zelabbas@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 09:05:36 by zelabbas          #+#    #+#             */
-/*   Updated: 2024/04/27 17:28:15 by zelabbas         ###   ########.fr       */
+/*   Updated: 2024/05/12 16:32:00 by zelabbas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 int main(int ac, char **av)
 {
 	int	i;
-	int	j;
+	std::string str;
 
 	if (ac == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
@@ -25,16 +25,15 @@ int main(int ac, char **av)
 		i = 1;
 		while (i < ac)
 		{
-			j = 0;
-			while (av[i][j])
+			str = av[i];
+			for (size_t j = 0; j < str.length() ; j++)
 			{
-				av[i][j] = std::toupper(av[i][j]);
-				j++;
+				if (!std::isupper(str.at(j)))
+					str.at(j) = std::toupper(str.at(j));
 			}
-			std::cout << av[i];
-			
+			std::cout << str;
 			i++;
 		}
-		std::cout << "\n";
+		std::cout << std::endl;
 	}
 }
