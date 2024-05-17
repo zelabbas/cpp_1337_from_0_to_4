409bfe414e7e8b4ac5ce33a7d8d689dfc02dac15
zelabbas <zakariaelabbassi2002@gmail.com>
2024-05-17 20:24:51 +0100

    update in getters

--- Module_01/ex00/Zombie.hpp
+++ Module_01/ex00/Zombie.hpp
@@ -6,7 +6,7 @@
 /*   By: zelabbas <zelabbas@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 20:37:23 by zelabbas          #+#    #+#             */
-/*   Updated: 2024/05/02 14:36:47 by zelabbas         ###   ########.fr       */
+/*   Updated: 2024/05/17 20:23:40 by zelabbas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,8 @@ class Zombie
 					Zombie();
 					~Zombie();
 		void		setName(std::string);
-		std::string	getName(void);
-		void announce(void);
+		std::string	getName(void) const;
+		void announce(void) const;
 };
 
 Zombie* newZombie( std::string name );
