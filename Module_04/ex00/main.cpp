8738f78f39a5f8d40e926068b2e1e78f5231fdc2
zelabbas <zakariaelabbassi2002@gmail.com>
2024-05-18 20:39:33 +0100

    main program to test all class

---
+++ Module_04/ex00/main.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zelabbas <zelabbas@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/18 15:36:08 by zelabbas          #+#    #+#             */
+/*   Updated: 2024/05/18 20:23:34 by zelabbas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Cat.hpp"
+#include "Dog.hpp"
+#include "WrongCat.hpp"
+
+int main()
+{
+	const Animal* meta = new Animal();
+	const Animal* j = new Dog("test");
+	const Animal* i = new Cat();
+	std::cout << j->getType() << " " << std::endl;
+	std::cout << i->getType() << " " << std::endl;
+	i->makeSound(); //will output the cat sound!
+	j->makeSound();
+	meta->makeSound();
+	delete i;
+	delete j;
+	delete meta;
+	std::cout << "---------------------------------------------" << std::endl;
+	const WrongAnimal* wrongAnimal = new WrongAnimal();
+	const WrongAnimal* wrongCat = new WrongCat();
+	std::cout << wrongAnimal->getType() << " " << std::endl;
+	std::cout << wrongCat->getType() << " " << std::endl;
+	wrongCat->makeSound();
+	wrongAnimal->makeSound();
+	delete wrongCat;
+	delete wrongAnimal;
+	return 0;
+}
