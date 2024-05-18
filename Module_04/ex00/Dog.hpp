9ef9819ccb4724c5cef8142ad1886821b644ad92
zelabbas <zakariaelabbassi2002@gmail.com>
2024-05-18 20:38:36 +0100

    define class Dog

---
+++ Module_04/ex00/Dog.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Dog.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zelabbas <zelabbas@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/18 14:57:13 by zelabbas          #+#    #+#             */
+/*   Updated: 2024/05/18 18:16:54 by zelabbas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DOG_HPP
+#define DOG_HPP
+
+#include "Animal.hpp"
+
+class Dog : public Animal
+{
+	private:
+		/* date */
+	public:
+		Dog(void);
+		Dog(const std::string&);
+		Dog(const Dog&);
+		~Dog();
+		
+		Dog& operator= (const Dog&);
+		void makeSound(void) const;
+};
+
+#endif
