997739bb12ef5597e8ae1c0f385875d11373454f
zelabbas <zakariaelabbassi2002@gmail.com>
2024-05-18 20:38:00 +0100

    define class cat

---
+++ Module_04/ex00/Cat.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Cat.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zelabbas <zelabbas@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/18 15:20:16 by zelabbas          #+#    #+#             */
+/*   Updated: 2024/05/18 18:17:09 by zelabbas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CAT_HPP
+#define CAT_HPP
+
+#include "Animal.hpp"
+
+class Cat : public Animal
+{
+	private:
+		/* data */
+	public:
+		Cat(void);
+		Cat(const std::string&);
+		Cat(const Cat&);
+		~Cat();
+		
+		Cat& operator= (const Cat&);
+		void makeSound(void) const;
+};
+
+#endif
