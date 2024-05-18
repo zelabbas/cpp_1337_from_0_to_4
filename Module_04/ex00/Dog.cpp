1a63bdaa278a701fdd4ccc627c5588c80f504cd3
zelabbas <zakariaelabbassi2002@gmail.com>
2024-05-18 20:38:59 +0100

    implements all methods in class Dog

---
+++ Module_04/ex00/Dog.cpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Dog.cpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zelabbas <zelabbas@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/18 15:04:45 by zelabbas          #+#    #+#             */
+/*   Updated: 2024/05/18 18:17:16 by zelabbas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Dog.hpp"
+
+Dog::Dog(void)
+{
+	std::cout << "Default Dog constructor called" << std::endl;
+	this->type = "Dog";
+}
+
+Dog::Dog(const std::string& type) : Animal(type)
+{
+	std::cout << "Default Dog constructor by name called" << std::endl;
+}
+
+Dog::Dog(const Dog& obj)
+{
+	std::cout << "copy Dog constructor called" << std::endl;
+	*this = obj;
+}
+
+Dog::~Dog(void)
+{
+	std::cout << "Dog Destructor called" << std::endl;
+}
+
+Dog&	Dog::operator=(const Dog& obj)
+{
+	std::cout << "Dog assignment operator called" << std::endl;
+	if (this != &obj)
+		Animal::operator= (obj);
+	return (*this);
+}
+
+void	Dog::makeSound(void) const
+{
+	std::cout << "Dog Sound..." << std::endl;
+}
