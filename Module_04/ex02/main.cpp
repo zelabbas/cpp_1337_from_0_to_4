c9fd1ffd01840a01c2d59bc47028e42d7f13161b
zelabbas <zakariaelabbassi2002@gmail.com>
2024-05-21 14:33:28 +0100

    add the main program to test the classes

---
+++ Module_04/ex02/main.cpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zelabbas <zelabbas@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/18 15:36:08 by zelabbas          #+#    #+#             */
+/*   Updated: 2024/05/21 14:27:56 by zelabbas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Cat.hpp"
+#include "Dog.hpp"
+#include "WrongCat.hpp"
+
+void leak(void) {system("leaks abstract");}
+int main()
+{
+	atexit(leak);
+	
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
+	std::cout << std::endl;
+	j->makeSound();
+	i->makeSound();
+	std::cout << std::endl;
+	delete j;
+	delete i;
+	std::cout << std::endl;
+
+	Animal* tab[4];
+
+	std::cout << std::endl;
+	
+	tab[0] = new Cat();
+	tab[1] = new Cat();
+	tab[2] = new Dog();
+	tab[3] = new Dog();
+	std::cout << std::endl;
+
+	for (size_t i = 0; i < 4; i++)
+		tab[i]->makeSound();
+	std::cout << std::endl;
+	
+	for (size_t i = 0; i < 4; i++)
+		delete tab[i];
+	return 0;
+}
