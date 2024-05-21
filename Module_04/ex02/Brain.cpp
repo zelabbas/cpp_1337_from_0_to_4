55d9f357067178b5fe8a41629aecf50fe4abaa4f
zelabbas <zakariaelabbassi2002@gmail.com>
2024-05-21 14:30:46 +0100

    implements all methods in Brain class

---
+++ Module_04/ex02/Brain.cpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Brain.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zelabbas <zelabbas@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/18 21:48:11 by zelabbas          #+#    #+#             */
+/*   Updated: 2024/05/19 12:16:34 by zelabbas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Brain.hpp"
+
+Brain::Brain(void)
+{
+	std::cout << "Default Brain constructor called" << std::endl;
+	for (size_t i = 0; i < 100; i++)
+		this->ideas[i] = "IDEA";
+}
+
+Brain::Brain(const Brain& obj)
+{
+	std::cout << "copy Brain constructor called" << std::endl;
+	*this = obj;
+}
+
+Brain::~Brain(void)
+{
+	std::cout << "Brain Destructor called" << std::endl;
+}
+
+Brain&	Brain::operator=(const Brain& obj)
+{
+	std::cout << "Brain assignment operator called" << std::endl;
+	if (this != &obj)
+	{
+		for (size_t i = 0; i < 100; i++)
+			this->ideas[i] = obj.ideas[i];
+	}
+	return (*this);
+}
