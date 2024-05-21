02c98f2e9099405d228fedfa85ea44db20f370ad
zelabbas <zakariaelabbassi2002@gmail.com>
2024-05-21 14:41:17 +0100

    implements all methods in class WrongCat

---
+++ Module_04/ex02/WrongCat.cpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   WrongCat.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zelabbas <zelabbas@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/18 18:20:17 by zelabbas          #+#    #+#             */
+/*   Updated: 2024/05/18 19:00:04 by zelabbas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "WrongCat.hpp"
+
+WrongCat::WrongCat(void)
+{
+	std::cout << "Default WrongCat constructor called" << std::endl;
+	this->type = "WrongCat";
+}
+
+WrongCat::WrongCat(const std::string& type) : WrongAnimal(type)
+{
+	std::cout << "Default WrongCat constructor by name called" << std::endl;
+}
+
+WrongCat::WrongCat(const WrongCat& obj)
+{
+	std::cout << "copy WrongCat constructor called" << std::endl;
+	*this = obj;
+}
+
+WrongCat::~WrongCat(void)
+{
+	std::cout << "WrongCat Destructor called" << std::endl;
+}
+
+WrongCat& WrongCat::operator=(const WrongCat& obj)
+{
+	std::cout << "WrongCat assignment operator called" << std::endl;
+	if (this != &obj)
+		WrongAnimal::operator=(obj);
+	return (*this);
+}
+
+void	WrongCat::makeSound(void) const
+{
+	std::cout << "WrongCat Sound..." << std::endl;
+}
