730115a3f16dfe6f0e8e6d5fddfb3a99a46936a1
zelabbas <zakariaelabbassi2002@gmail.com>
2024-05-25 20:55:26 +0100

    define IMateriaSource class

---
+++ Module_04/ex03/IMateriaSource.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*    IMateriaSource.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zelabbas <zelabbas@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/25 10:56:33 by zelabbas          #+#    #+#             */
+/*   Updated: 2024/05/25 11:00:01 by zelabbas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef IMATERIASOURCE_HPP
+#define IMATERIASOURCE_HPP
+
+#include "AMateria.hpp"
+
+class IMateriaSource
+{
+	public:
+	virtual ~IMateriaSource() {}
+	virtual void learnMateria(AMateria*) = 0;
+	virtual AMateria* createMateria(std::string const & type) = 0;
+};
+
+#endif
