ee1f46f2d563fc5c30cf85c263a04d922e9d86d9
zelabbas <zakariaelabbassi2002@gmail.com>
2024-05-26 10:04:48 +0100

    update the testes

--- Module_04/ex03/main.cpp
+++ Module_04/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: zelabbas <zelabbas@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 15:19:11 by zelabbas          #+#    #+#             */
-/*   Updated: 2024/05/25 20:42:55 by zelabbas         ###   ########.fr       */
+/*   Updated: 2024/05/26 10:04:27 by zelabbas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,10 @@
 #include "Character.hpp"
 #include "MateriaSource.hpp"
 
-void leak(){system("leaks interfaces");}
+// void leak(){system("leaks interfaces");}
 int main()
 {
-	atexit(leak);
+	// atexit(leak);
 	IMateriaSource* src = new MateriaSource();
 	src->learnMateria(new Ice());
 	src->learnMateria(new Cure());
@@ -29,15 +29,34 @@ int main()
 	me->equip(tmp);
 	tmp2 = src->createMateria("cure");
 	me->equip(tmp2);
-	me->unequip(0);
 	ICharacter* bob = new Character("bob");
 	me->use(0, *bob);
 	me->use(1, *bob);
-	me->use(2, *bob);
+	std::cout << std::endl;
+
+	me->use(6, *bob);
+
+	me->unequip(1);
+	me->use(0, *bob);
+	me->use(1, *bob);
+
+	std::cout << std::endl;
+
+	me->unequip(1);
+	me->use(0, *bob);
+	me->use(1, *bob);
+
+	std::cout << std::endl;
+
+	me->unequip(0);
+	me->use(0, *bob);
+	me->use(1, *bob);
+
 	delete bob;
 	delete me;
 	delete src;
 	delete tmp;
+	delete tmp2;
 	return 0;
 
 	return 0;
